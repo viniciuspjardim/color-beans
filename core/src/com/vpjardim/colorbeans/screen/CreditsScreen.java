b6d4c7495241bfd4b32054eacdaf9e514044d894
Vinícius Jardim <viniciuspjardim@gmail.com>
2024-06-01 17:33:08 -0300

    Hide content to display background tapping on CreditsScreen

--- core/src/com/vpjardim/colorbeans/screen/CreditsScreen.java
+++ core/src/com/vpjardim/colorbeans/screen/CreditsScreen.java
@@ -4,12 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.vpjardim.colorbeans.G;
 import com.vpjardim.colorbeans.events.Event;
 import com.vpjardim.colorbeans.events.EventHandler;
+import com.vpjardim.colorbeans.events.EventListener;
 
 public class CreditsScreen extends ScreenBase {
+    private EventListener tapInput;
+
     @Override
     public void show() {
         super.show();
@@ -34,6 +38,10 @@ public class CreditsScreen extends ScreenBase {
 
         containerT.add(contentT);
 
+        // Tap event
+        tapInput = (Event e) -> containerT.setVisible(!containerT.isVisible());
+        EventHandler.get().addListener("TapInput.tap", tapInput);
+
         Label.LabelStyle keyLS = G.game.skin.get("labelWhite50", Label.LabelStyle.class);
         Label.LabelStyle valueRegLS = G.game.skin.get("labelDef", Label.LabelStyle.class);
         Label.LabelStyle valueLS = G.game.skin.get("labelCredits", Label.LabelStyle.class);
@@ -42,49 +50,38 @@ public class CreditsScreen extends ScreenBase {
         repositoryL.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                containerT.setVisible(true);
                 Gdx.net.openURI("https://github.com/viniciuspjardim/color-beans");
             }
         });
 
-        Label backL = new Label("** Press any key or click here to go back **", keyLS);
-        backL.addListener(new ClickListener() {
+
+        final TextButton backBtt = new TextButton("Back",
+                G.game.skin.get("bttYellow", TextButton.TextButtonStyle.class));
+        backBtt.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 action = ScreenBase.ACT_NEXT;
             }
         });
 
-        contentT.add(new Label("Code and art:", keyLS));
-        contentT.row();
-        contentT.add(new Label("Vinícius Jardim", valueLS)).padTop(8);
-        contentT.row();
+        contentT.add(new Label("Code, art and tests:", keyLS)).row();
+        contentT.add(new Label("Vinícius Jardim", valueLS)).padTop(8).row();
 
-        contentT.add(new Label("Tests:", keyLS)).padTop(32);
-        contentT.row();
-        contentT.add(new Label("Anaiara Silva", valueLS)).padTop(8);
-        contentT.row();
-        contentT.add(new Label("Vinícius Jardim", valueLS)).padTop(8);
-        contentT.row();
-        contentT.add(new Label("André Jardim", valueLS)).padTop(8);
-        contentT.row();
+        contentT.add(new Label("More tests:", keyLS)).padTop(32).row();
+        contentT.add(new Label("Anaiara Silva", valueLS)).padTop(8).row();
+        contentT.add(new Label("André Jardim", valueLS)).padTop(8).row();
 
-        contentT.add(new Label("Color Beans is a clone from:", keyLS)).padTop(32);
-        contentT.row();
-        contentT.add(new Label("SEGA's Puyo Puyo", valueRegLS)).padTop(8);
-        contentT.row();
+        contentT.add(new Label("Color Beans is a clone from:", keyLS)).padTop(32).row();
+        contentT.add(new Label("SEGA's Puyo Puyo", valueRegLS)).padTop(8).row();
 
-        contentT.add(new Label("More info at:", keyLS)).padTop(32);
-        contentT.row();
-        contentT.add(repositoryL).padTop(8);
-        contentT.row();
+        contentT.add(new Label("More info at:", keyLS)).padTop(32).row();
+        contentT.add(repositoryL).padTop(8).row();
 
-        contentT.add(new Label("****", keyLS)).padTop(32);
-        contentT.row();
+        contentT.add(new Label("****", keyLS)).padTop(32).row();
 
-        contentT.add(new Label("Thanks for playing!", valueLS)).padTop(32);
-        contentT.row();
-        contentT.add(backL).padTop(8);
-        contentT.row();
+        contentT.add(new Label("Thanks for playing!", valueLS)).padTop(32).row();
+        contentT.add(backBtt).width(G.style.buttWidth).padTop(48);
 
         stage.addActor(containerT);
 
@@ -107,35 +104,6 @@ public class CreditsScreen extends ScreenBase {
     @Override
     public void dispose() {
         super.dispose();
-    }
-
-    @Override
-    public void keyDown(int key) {
-        action = ScreenBase.ACT_NEXT;
-    }
-
-    @Override
-    public void btStartDown() {
-        action = ScreenBase.ACT_NEXT;
-    }
-
-    @Override
-    public void bt1Down() {
-        action = ScreenBase.ACT_NEXT;
-    }
-
-    @Override
-    public void bt2Down() {
-        action = ScreenBase.ACT_NEXT;
-    }
-
-    @Override
-    public void bt3Down() {
-        action = ScreenBase.ACT_NEXT;
-    }
-
-    @Override
-    public void bt4Down() {
-        action = ScreenBase.ACT_NEXT;
+        EventHandler.get().removeListener("TapInput.tap", tapInput);
     }
 }
